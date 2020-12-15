10d940a5cc658bfb8801925e6a026068fa784837
jdhofmann <justindhofmann@gmail.com>
2020-12-15 14:18:50 -0500

    added in answers to index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,10 @@ class QuestionsController < ApplicationController
     def index 
         questions = Question.all 
 
-        render json: questions
+        answers = 
+
+        render json: questions.to_json(only: [:text, :id, :category, :required_yes_id], include: [:answers ])
+
+
     end 
 end
